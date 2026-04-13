1a58537a26140833fe321c5a9dfa49c839879f86
TravelNurseIntel <topchoice.gwendolyn@outlook.com>
2026-04-13 13:21:52 -0500

    Create db.js

---
+++ api/db.js
@@ -0,0 +1,24 @@
+import pkg from 'pg'
+
+const { Pool } = pkg
+
+const pool = new Pool({
+connectionString: process.env.DATABASE_URL
+})
+
+export async function query(text, params) {
+
+const client = await pool.connect()
+
+try {
+
+const result = await client.query(text, params)
+return result
+
+} finally {
+
+client.release()
+
+}
+
+}
